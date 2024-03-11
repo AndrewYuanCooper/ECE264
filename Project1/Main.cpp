97feee14a40da288bc0867c8942390881313cb5c
AndrewYuanCooper <andrew.yuan@cooper.edu>
2024-03-11 19:11:09 -0400

    changed namings

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -9,26 +9,24 @@ using namespace std;
 
 int main() {
 //Enter input file
-string filenameInput;
-string filenameOutput;
-string file_name;
-
+string file_Input;
+string file_Output;
 
 cout << "Please enter in an input file: " << "\n"; 
-cin >> file_name; 
+cin >> file_Input; 
 cout << "Enter the name of the output file: ";
-cin >> filenameOutput;
+cin >> file_Output;
 
 ifstream input;
 ofstream output;
 
-input.open (filenameInput);
-output.open (filenameOutput);
+input.open (file_Input);
+output.open (file_Output);
 
 vector<foobar*> v;
 read_file(input, v);
 Foobar_positions(v);
-write_file(filenameOutput, v);
+write_file(file_Output, v);
 
 //Closing input and output files
    input.close();
